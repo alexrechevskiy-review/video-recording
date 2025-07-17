d6aeb3521ffff4a4e0a15e2d9902b38b55b48c7b
bm48 <muldersbert48@gmail.com>
2025-07-17 21:50:49 +0200

    fix: thanks page

--- app/(dashboard)/thanks/page.tsx
+++ app/(dashboard)/thanks/page.tsx
@@ -14,33 +14,31 @@ const Thanks = () => {
     router.push("/history");
   };
   return (
-    <main className="container mx-auto p-6 max-w-7xl">
-      <div className="mb-8 w-full flex items-center justify-between gap-4">
-        <Button
-          onClick={handleBack}
-          variant="secondary"
-          >
-          Record another video
-        </Button>
-        <Button
-          variant="outline"
-          onClick={handleHistory}
-        >
-          Your Submissions
-        </Button>
-      </div>
-      <div className="text-center mb-4">
-        <div className="inline-flex items-center justify-center w-12 h-12 rounded-full bg-green-100 mb-4">
+    <main className="container m-auto p-6 max-w-7xl flex flex-col items-center justify-center min-h-[70vh]">
+      <div className="flex flex-col items-center justify-center w-full">
+        <div className="flex items-center justify-center w-12 h-12 rounded-full bg-green-100 mb-4">
           <Check className="w-6 h-6 text-green-600" />
         </div>
-        <h3 className="text-xl font-medium mb-2">Submission Complete!</h3>
-        <p className="text-gray-600 mb-4">Your video has been uploaded successfully and will be reviewed soon.</p>
-        <Button
-          onClick={handleBack}
-          variant="secondary"
-        >
-          Record another video
-        </Button>
+        <h3 className="text-xl font-medium mb-2 text-center">Submission Complete!</h3>
+        <p className="text-gray-600 mb-4 text-center">
+          Your video has been uploaded successfully and will be reviewed soon.
+        </p>
+        <div className="flex gap-4 md:flex-row flex-col justify-center items-center">
+          <Button
+            variant="outline"
+            onClick={handleHistory}
+            className="w-48"
+          >
+            Your Submissions
+          </Button>
+          <Button
+            onClick={handleBack}
+            variant="secondary"
+            className="w-48"
+          >
+            Record another video
+          </Button>
+        </div>
       </div>
     </main>
   )
